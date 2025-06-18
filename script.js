789e15e387b23bc8ea0b50637e03ff94a4412a04
Anna Poga <anna.poga988@gmail.com>
2025-06-18 18:08:09 +0300

    Added more tasks

--- script.js
+++ script.js
@@ -1,33 +1,84 @@
-const contents = document.querySelector('.contents');
+const calculator_plus = document.querySelector('.calculator-plus');
+const calculator_minus = document.querySelector('.calculator-minus');
+const letter_capitalizer = document.querySelector('.letter-capitalizer');
 
-const elements = {
-    input_1: contents.querySelector('input[name="input_1"]'),
-    input_2: contents.querySelector('input[name="input_2"]'),
-    calculate: contents.querySelector('button[name="calculate"]'),
-    result: contents.querySelector('.result'),
-    error: contents.querySelector('.error')
-};
+calculator_plus.style.display = 'none';
+calculator_minus.style.display = 'none';
+letter_capitalizer.style.display = 'none';
 
-elements.calculate.addEventListener('click', e => {
-    elements.error.textContent = '';
+document.querySelector('.navigation .one').addEventListener('click', () => {
+    calculator_plus.style.display = '';
+    calculator_minus.style.display = 'none';
+    letter_capitalizer.style.display = 'none';
+});
+document.querySelector('.navigation .two').addEventListener('click', () => {
+    calculator_plus.style.display = 'none';
+    calculator_minus.style.display = '';
+    letter_capitalizer.style.display = 'none';
+});
+document.querySelector('.navigation .three').addEventListener('click', () => {
+    calculator_plus.style.display = 'none';
+    calculator_minus.style.display = 'none';
+    letter_capitalizer.style.display = '';
+});
 
-    const number_1 = Number(elements.input_1.value);
-    const number_2 = Number(elements.input_2.value);
+calculator_plus.querySelector('button[name="calculate"]').addEventListener('click', () => {
+    const error = calculator_plus.querySelector('.error');
+
+    error.textContent = '';
+
+    const number_1 = Number(calculator_plus.querySelector('input[name="input_1"]').value);
+    const number_2 = Number(calculator_plus.querySelector('input[name="input_2"]').value);
 
     let result = '';
 
     if (isNaN(number_1) || isNaN(number_2)) {
-        elements.error.textContent = 'Only numbers are allowed!';
+        error.textContent = 'Only numbers are allowed!';
     }
     else if (number_1 < 0 || number_2 < 0) {
-        elements.error.textContent = 'Negative numbers are not allowed!';
+        error.textContent = 'Negative numbers are not allowed!';
     }
     else if (number_1 > 9999 || number_2 > 9999) {
-        elements.error.textContent = 'One or both fields are out of range (0 - 9999). Please enter valid numbers!';
+        error.textContent = 'One or both fields are out of range (0 - 9999). Please enter valid numbers!';
     }
     else {
         result = number_1 + number_2;
     }
 
-    elements.result.textContent = result;
+    calculator_plus.querySelector('.result').textContent = result;
+});
+
+calculator_minus.querySelector('button[name="calculate"]').addEventListener('click', () => {
+    const error = calculator_minus.querySelector('.error');
+
+    error.textContent = '';
+
+    const number_1 = Number(calculator_minus.querySelector('input[name="input_1"]').value);
+    const number_2 = Number(calculator_minus.querySelector('input[name="input_2"]').value);
+
+    let result = '';
+
+    if (isNaN(number_1) || isNaN(number_2)) {
+        error.textContent = 'Only numbers are allowed!';
+    }
+    else if (number_1 < 0 || number_2 < 0) {
+        error.textContent = 'Negative numbers are not allowed!';
+    }
+    else {
+        result = number_1 - number_2;
+    }
+
+    calculator_minus.querySelector('.result').textContent = result;
+});
+
+letter_capitalizer.querySelector('button[name="capitalize"]').addEventListener('click', () => {
+    const value = letter_capitalizer.querySelector('input[name="input"]').value;
+
+    let result = '-';
+
+    if (isNaN(value)) {
+        result = value.toUpperCase();
+    }
+
+    letter_capitalizer.querySelector('.result').textContent = result;
 });
